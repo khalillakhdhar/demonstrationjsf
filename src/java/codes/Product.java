ddf6c708feb63a487dd4e7ffdae6d606ae7b1185
khalil <khalillakhdharatc@gmail.com>
2022-02-28 15:29:45 +0100

    Initial commit

---
+++ src/java/codes/Product.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codes;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author khali
+ */
+@ManagedBean
+@RequestScoped
+public class Product {
+    private String productName="T-shirt";
+    private int productPrice;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    }
+    
+    
+    
+}
